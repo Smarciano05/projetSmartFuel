981a67fa40685bcf14e5caff64c81ef1c08a3335
09-12-96 <marecarsarah@gmail.com>
2026-05-01 15:11:13 +0200

    commentaire

--- src/Controller/StockCarburantController.php
+++ src/Controller/StockCarburantController.php
@@ -6,7 +6,7 @@ use App\Entity\Pompiste;
 use App\Entity\StockCarburant;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
-use App\Repository\StockCarburantRepository;+
+use App\Repository\StockCarburantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
