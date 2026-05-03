ff41dfd1d0ad1b11487a55e97f7d8c49db89657c
09-12-96 <marecarsarah@gmail.com>
2026-05-03 20:31:06 +0200

    page copy right + modification du css de recherche_plaque

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,11 +8,24 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
+    //CHEMIN vers la page d'Accueil
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+    // CHEMIN vers la page "à propos"
+    #[Route('/a-propos', name: 'app_about')]
+    public function about(): Response  
+    {
+        return $this->render('about/index.html.twig');
+    }
+    // CHEMIN vers la page "copy right"
+    #[Route('/copy_right', name: 'app_right')]
+    public function copyRight(): Response  
+    {
+        return $this->render('copyright.html.twig');
     }
 }
